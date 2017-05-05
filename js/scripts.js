ee226a613403a6c2278b92de4af065c0868c0de8
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-05 10:21:47 -0700

    Modify if else statement that takes numbers divisible by both 3 and 5 and replaces them with ping-pong.

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,12 @@ function playPingPong(number){
   }
 
   for(var i = 1; i <= number; i++) {
-    if (i % 3 === 0) {
-      result.push("ping");
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      result.push("ping-pong");
     } else if (i % 5 === 0) {
       result.push("pong");
+    } else if (i % 3 === 0) {
+        result.push("ping");
     } else {
       result.push(i);
     }
